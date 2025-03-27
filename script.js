d9834655e5ba0d0e466f4821aa577bac569aeceb
Ankit singh <149090246+ankitsingh7777903679@users.noreply.github.com>
2025-03-28 01:57:01 +0530

    Update BCA Semester 4 link to use relative path for solution page

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ const bcaLinks = [
   "https://questionbanker.in/bca_sem1",
   "https://questionbanker.in/bca_sem2",
   "https://questionbanker.in/bca_sem3",
-  "https://questionbanker.in/bca-sem4-solution",
+  "bca-sem4-solution.html",
   "https://questionbanker.in/bca_sem5",
   "https://questionbanker.in/bca_sem6"
 ];
